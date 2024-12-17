f49844fef8ef191103bb3415e4ffba1285b85f73
developer <developer@local>
2024-12-17 18:20:46 +0100

    Pridanie podpory pre odhlasenie z notifikacii

---
+++ src/NotificationService/Migrations/20241217172023_V02.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotificationService.Migrations
+{
+    /// <inheritdoc />
+    public partial class V02 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NotificationSettings",
+                columns: table => new
+                {
+                    Email = table.Column<string>(type: "varchar(255)", nullable: false),
+                    AuthKey = table.Column<string>(type: "varchar(255)", nullable: false),
+                    IsDisabled = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotificationSettings", x => x.Email);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NotificationSettings_AuthKey",
+                table: "NotificationSettings",
+                column: "AuthKey");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NotificationSettings");
+        }
+    }
+}
